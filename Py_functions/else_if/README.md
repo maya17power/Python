350b62d26d457d5c18cf7f1fc772a9122f134a1c
maya17power <tavo1.ortega1@gmail.com>
2017-09-10 22:26:34 -0700

    updating date time function.

---
+++ Py_functions/else_if/README.md
@@ -0,0 +1,1 @@
+The else statement complements the if statement. An if/else pair says: "If this expression is true, run this indented code block; otherwise, run this code after the else statement."
---
+++ Py_functions/else_if/else_if.py
@@ -0,0 +1,25 @@
+answer = "'Tis but a scratch!"
+
+def black_knight():
+    if answer == "'Tis but a scratch!":
+        return True
+    else:
+        return False       # Make sure this returns False
+
+def french_soldier():
+    if answer == "Go away, or I shall taunt you a second time!":
+        return True
+    else:
+        return False      # Make sure this returns False
+
+        def greater_less_equal_5(answer):
+    if answer > 5:
+        return 1
+    elif answer < 5:
+        return -1
+    else:
+        return 0
+
+print greater_less_equal_5(4)
+print greater_less_equal_5(5)
+print greater_less_equal_5(6)
