a736a680269276f4d85cc7874a3fab25d3aea426
maya17power <tavo1.ortega1@gmail.com>
2017-09-10 22:28:51 -0700

    Adding datetime to repo.

--- Py_functions/datetime_now/datetime_now.py
+++ Py_functions/datetime_now/datetime_now.py
@@ -8,3 +8,9 @@ print now
 print now.year
 print now.month
 print now.day
+
+#display the date dd/mm/yyyy using string substitution
+print "Todays date is %s/%s/%s" % (now.day,now.month,now.year)
+
+#display the time in military 24hr format
+print "Current time is %s:%s:%s" % (now.hour,now.minute,now.second)
